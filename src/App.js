c3911b2648eba5f0df82e2fd99c273a997257a3c
Gabriela Salazar <asmgabriela@gmail.com>
2022-09-24 21:07:24 -0500

    feat: add router in react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { useSelector } from "react-redux";
 import { selectUser } from "./features/userSlice";
 import Registro from "./features/pages/registro/Registro";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const user = useSelector(selectUser);
@@ -13,16 +14,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {user ? (
-          <>
-            <h2>Usuario logeado</h2>
-          </>
-        ) : (
-          <>
-            <Registro isRegistered={isRegistered} />
-          </>
-        )}
       </header>
+      <Routes>
+        <Route
+          path="register"
+          element={
+            user ? (
+              <>
+                <h2>Usuario logeado</h2>
+              </>
+            ) : (
+              <Registro isRegistered={isRegistered} />
+            )
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
